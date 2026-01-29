1e45e791953ffc46c23e5e5d352703597ce4d838
Sandaru Wijewardhana <150885140+sandaruwijewardhana@users.noreply.github.com>
2026-01-29 21:38:32 +0530

    comment corrected

--- lAB06/Task05.py
+++ lAB06/Task05.py
@@ -1,10 +1,8 @@
 def infinite_recursion():
     """A recursive function with no base case."""
     print("Calling function...")
-    # Recursive Case: Function calls itself without a stopping condition
+   
     return infinite_recursion()
-
-# Executing the function
 try:
     infinite_recursion()
 except RecursionError as e:
